4f179fbc39154e118a0a39abdbf225995c1ec317
preaus007 <touhedcs@gmail.com>
2024-06-28 11:32:32 +0600

    Do navbar responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import logo from "../assets/logo.png"
 import { navItems } from "../constants/index"
-
+import { X, Menu, MenuIcon } from 'lucide-react'
+import { useState } from 'react'
+ 
 const Navbar = () => {
+    const [mobileDrawerOpen, setmobileDrawerOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setmobileDrawerOpen(!mobileDrawerOpen);
+    }
+
   return (
     <nav className="sticky top-0 z-50 py-3 backdeop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm">
@@ -28,9 +36,35 @@ const Navbar = () => {
                     <a href="#" className="py-2 px-3 border rounded-md bg-gradient-to-r from-orange-500 to-orange-800">
                         Create an account
                     </a>
-                    
+                </div>
+
+                <div className="lg:hidden md:flex flex-column justify-end">
+                    <button onClick={toggleNavbar}>
+                        {mobileDrawerOpen ? <X /> : <MenuIcon/>}
+                    </button>
                 </div>
             </div>
+
+            {mobileDrawerOpen && (
+                <div className="fixed right-0 z-20 bg-neutral-900 w-full p-10 flex flex-col justify-center items-center lg:hidden">
+                    <ul>
+                        {navItems.map((item, index) => (
+                            <li className='py-2' key={index}>
+                                <a href={item.href}>{item.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+
+                    <div className="flex space-x-6 mt-3">
+                        <a href="#" className="py-2 px-3 border rounded-md">
+                            Sign In
+                        </a>
+                        <a href="#" className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800">
+                            Create an account
+                        </a>
+                        </div>
+                </div>
+            )}
         </div>
     </nav>
   )
